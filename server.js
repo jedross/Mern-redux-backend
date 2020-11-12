350d2dbe2fe8691f2f332c533a2c17ac7a3b79b3
Jeffrey Ross <jeffreyross@Jeffreys-MacBook-Pro.local>
2020-11-12 08:29:03 -0700

    Server update

--- server.js
+++ server.js
@@ -1,36 +1,36 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const morgan = require('morgan')
-const bodyParser = require('body-parser')
-const cors = require('cors')
-require('dotenv').config()
+const express = require("express");
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+require("dotenv").config();
 
-//app
-const app = express()
+// app
+const app = express();
 
 // db
-mongoose.connect(process.env.DATABASE, {
-    useNewUrlParser:true,
-    useCreateIndex:true,
-    useFindAndModify: true
-})
-.then(() => console.log("DB CONNECTED"))
-.catch(err => console.log(`DB CONNECTION ERR ${err}`))
+mongoose
+  .connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: true,
+  })
+  .then(() => console.log("DB CONNECTED"))
+  .catch((err) => console.log("DB CONNECTION ERR", err));
 
-//middlewares
+// middlewares
 app.use(morgan("dev"));
-app.use(bodyParser.json({limit: "2mb"}))
-app.use(cors())
+app.use(bodyParser.json({ limit: "2mb" }));
+app.use(cors());
 
+// route
+app.get("/api", (req, res) => {
+  res.json({
+    data: "hey you hit node API",
+  });
+});
 
-//route
-app.get('/api', (req,res) => {
-    res.json({
-        data: "you hit node API"
-    })
-})
+// port
+const port = process.env.PORT || 8000;
 
-//port
-const port = process.env.PORT || 8000
-
-app.listen(port, () => console.log(`Server is running on port ${port}`))
+app.listen(port, () => console.log(`Server is running on port ${port}`));
